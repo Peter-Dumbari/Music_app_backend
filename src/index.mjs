84cd8ed4f131dfb953ce99b312618c14d81bd2e7
Peter Dumbari <dummytwiz@gmail.com>
2024-09-11 17:45:16 +0100

    initial commit for middleware

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,10 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+const loggerMiddleWare = (req, res, next) => {
+  console.log(`${req.method}-${req.path}`);
+  next();
+};
 
 app.get("/", (req, res) => {
   res.status(201).send({ msg: "Hello World" });
