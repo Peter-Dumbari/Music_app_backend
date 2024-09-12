0ae997d1267ab7d1038aabf4b4126393677869ac
Peter Dumbari <dummytwiz@gmail.com>
2024-09-12 17:50:10 +0100

    validation of query param sucessfully

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import { query, validationResult } from "express-validator";
 
 const app = express();
 app.use(express.json());
@@ -38,23 +39,34 @@ const mockUser = [
   { id: 2, name: "jane", displayName: "Jane" },
 ];
 
-app.get("/api/v1/users", (req, res) => {
-  const {
-    query: { filter, value },
-  } = req;
+app.get(
+  "/api/v1/users",
+  query("filter")
+    .isString()
+    .withMessage("filter must be a string")
+    .notEmpty()
+    .withMessage("Must not be empty")
+    .isLength({ min: 3, max: 10 })
+    .withMessage("filter must be a string and must be between 3-10 characters"),
+  (req, res) => {
+    const {
+      query: { filter, value },
+    } = req;
+    const result = validationResult(req);
+    console.log("result", result);
+    if (!value && !filter) {
+      return res.status(200).send(mockUser);
+    }
 
-  if (!value && !filter) {
-    return res.status(200).send(mockUser);
+    if (value && filter) {
+      const filteredUsers = mockUser.filter((user) =>
+        user[filter].includes(value)
+      );
+      return res.status(200).send(filteredUsers);
+    }
+    res.status(201).send(mockUser);
   }
-
-  if (value && filter) {
-    const filteredUsers = mockUser.filter((user) =>
-      user[filter].includes(value)
-    );
-    return res.status(200).send(filteredUsers);
-  }
-  res.status(201).send(mockUser);
-});
+);
 
 //sending data function
 
