fa9642b3e66d35e02e172a0c5fab6516d71b328d
Peter Dumbari <dummytwiz@gmail.com>
2024-09-16 23:09:12 +0100

    installed cookies-parser to get access to cookies

--- src/routes/product.mjs
+++ src/routes/product.mjs
@@ -3,12 +3,15 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/v1/products", (req, res) => {
-  console.log(req.headers.cookie);
-  res.status(201).send([
-    { id: 0, name: "product 1" },
-    { id: 1, name: "product 2" },
-    { id: 2, name: "product 3" },
-  ]);
+  console.log("cookies", req.cookies);
+  if (req.cookies.hello && req.cookies.hello === "world") {
+    return res.status(201).send([
+      { id: 0, name: "product 1" },
+      { id: 1, name: "product 2" },
+      { id: 2, name: "product 3" },
+    ]);
+  } else
+    return res.status(400).send("you need to have correct cookie to see this");
 });
 
 export default router;
