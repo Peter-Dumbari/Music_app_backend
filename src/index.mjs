3c5f5dfa116a3fa843fdf9e7e3f1d5eef275e7f9
Peter Dumbari <dummytwiz@gmail.com>
2024-09-17 15:05:14 +0100

    implememted the first part of session lesson

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,22 @@
 import express from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "Peter the dev",
+    saveUninitialized: false,
+    resave: false,
+
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  })
+);
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
@@ -16,6 +28,7 @@ const loggerMiddleWare = (req, res, next) => {
 // app.use(loggerMiddleWare); // this will log all the request
 
 app.get("/", (req, res) => {
+  req.session.visited = true;
   res.cookie("hello", "world", {
     maxAge: 10000,
   });
