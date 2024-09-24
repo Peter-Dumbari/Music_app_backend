02c3ab123e34ce58187fd8693b8c3db33cf2e025
Peter Dumbari <dummytwiz@gmail.com>
2024-09-24 12:44:52 +0100

    implemented authenticating with db datas

--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -1,37 +1,65 @@
 import passport from "passport";
 import { Strategy } from "passport-local";
 import { mockUser } from "../utils/constants.mjs";
+import { User } from "../mongoose/schema/user.mjs";
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
+// passport.deserializeUser((id, done) => {
+//   try {
+//     const findUser = mockUser.find((user) => user.id === id);
+//     if (!findUser) throw new Error("User not found");
+//     done(null, findUser);
+//   } catch (err) {
+//     done(err, null);
+//   }
+// });
+
+passport.deserializeUser(async (id, done) => {
   try {
-    const findUser = mockUser.find((user) => user.id === id);
+    const findUser = await User.findById(id);
     if (!findUser) throw new Error("User not found");
     done(null, findUser);
-  } catch (err) {
-    done(err, null);
+  } catch (error) {
+    done(error, null);
   }
 });
 
+// export default passport.use(
+//   new Strategy((username, password, done) => {
+//     console.log("password", password);
+//     console.log("username", username);
+//     try {
+//       const findUser = mockUser.find((user) => user.name === username);
+
+//       if (!findUser) {
+//         throw new Error("user not found");
+//       }
+
+//       if (findUser.password !== password)
+//         throw new Error("Invalid Credentials");
+//       done(null, findUser);
+//     } catch (err) {
+//       done(err, null);
+//     }
+//   })
+// );
+
 export default passport.use(
-  new Strategy((username, password, done) => {
-    console.log("password", password);
+  new Strategy(async (username, password, done) => {
     console.log("username", username);
+    console.log("password", password);
+
     try {
-      const findUser = mockUser.find((user) => user.name === username);
-
-      if (!findUser) {
-        throw new Error("user not found");
-      }
-
+      const findUser = await User.findOne({ username });
+      if (!findUser) return res.status(404).send({ msg: "User not found" });
       if (findUser.password !== password)
         throw new Error("Invalid Credentials");
       done(null, findUser);
-    } catch (err) {
-      done(err, null);
+    } catch (error) {
+      done(error, null);
     }
   })
 );
