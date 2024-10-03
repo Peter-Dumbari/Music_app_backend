8697c3701fc1e1cc4f5ed944d2f93c7b1488099b
Peter Dumbari <dummytwiz@gmail.com>
2024-10-03 05:05:37 +0100

    implemented user creation

---
+++ models/userModel.mjs
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+// Declare the Schema of the Mongo model
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Export the model as default
+export default mongoose.model("User", userSchema);
